a706fad0dac3d369f740b3f551d77d20af97ff67
Chris Speck <cgspeck@gmail.com>
2019-04-10 17:45:11 +1000

    initial commit

---
+++ src/states.h
@@ -0,0 +1,12 @@
+#ifndef states_h
+#define states_h
+
+enum class State {
+        starting_up,
+        idle,
+        first_setup_latitude,
+        setup_latitude,
+        setup_intervalometer_meter
+    };
+
+#endif
