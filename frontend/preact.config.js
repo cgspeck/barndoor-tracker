c8490dab0f792a6264e2ce106e597f6fc78e942c
Chris Speck <cgspeck@gmail.com>
2019-11-28 17:21:48 +1100

    Dont build unnecessary things and reduce file name lengths

--- frontend/preact.config.js
+++ frontend/preact.config.js
@@ -1,41 +1,13 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
 export default (config, env, helpers) => {
-	if (env.isProd) {
-		config.devtool = false;
-		config.output.filename = `[contenthash].js`;
-		config.output.chunkFilename = `[contenthash].js`;
-	}
+  if (env.isProd) {
+    config.output.filename = (chunkData) => {
+      return `${chunkData.chunk.name.slice(0, 6)}.js`
+    };
+    config.output.chunkFilename = `[id].c.js`;
 
-	// config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: `*.*` }]) );
-	config.plugins.push( new MiniCssExtractPlugin({
-		filename: `[contenthash].css`,
-		chunkFilename: `[contenthash].css`
-	}));
-	// config.module.loaders.push({ test: /\.css$/, loader: ['mini-css-extract-plugin']});
-
-	/*module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: (resourcePath, context) => {
-                // publicPath is the relative path of the resource to the context
-                // e.g. for ./css/admin/main.css the publicPath will be ../../
-                // while for ./css/main.css the publicPath will be ../
-                return path.relative(path.dirname(resourcePath), context) + '/';
-              },
-            },
-          },
-          'css-loader',
-        ],
-      },
-    ],
-	},*/
-	// let { rules } = helpers.getRules;
-	// rules.push( new RuleWrapper)
+    let cssPlugin = helpers.getPluginsByName(config, "MiniCssExtractPlugin")[0].plugin;
+    cssPlugin.options.filename = `[contenthash:5].css`;
+    cssPlugin.options.chunkFilename = `[contenthash:5].c.css`;
+  }
 };
 
