e7aae771d1afb5f556119958a5262710722956af
Chris Speck <cgspeck@gmail.com>
2019-12-02 12:30:31 +1100

    Settle on CamelCase and allow alt and az error to be set

--- frontend/src/lib/settings/index.js
+++ frontend/src/lib/settings/index.js
@@ -1,43 +1,58 @@
-import axios from 'axios';
-import config from '../../config';
+import axios from "axios";
+import config from "../../config";
 
 async function getAllSettings() {
-  return axios.get(`${config.endpoint}/settings/debug`)
-    .then(r => r.data);
+  return axios.get(`${config.endpoint}/settings/debug`).then(r => r.data);
 }
 
 async function getAPSettings() {
-  return axios.get(`${config.endpoint}/settings/ap`)
-    .then(r => r.data);
+  return axios.get(`${config.endpoint}/settings/ap`).then(r => r.data);
 }
 
 async function setAPSettings(ssid, key) {
-  return axios.post(`${config.endpoint}/settings/ap`, {
-    ssid: ssid,
-    key: key
-  })
-  .then(r => r.data);
+  return axios
+    .post(`${config.endpoint}/settings/ap`, {
+      ssid: ssid,
+      key: key
+    })
+    .then(r => r.data);
 }
 
 async function getLocationSettings() {
-  return axios.get(`${config.endpoint}/settings/location`)
-    .then(r => r.data);
+  return axios.get(`${config.endpoint}/settings/location`).then(r => r.data);
 }
 
-async function setLocationSettings(latitude, magDeclination, x_offset, y_offset, z_offset) {
-  return axios.post(`${config.endpoint}/settings/location`, {
-    latitude: latitude,
-    magDeclination: magDeclination,
-    x_offset: x_offset,
-    y_offset: y_offset,
-    z_offset: z_offset
-  })
-  .then(r => r.data);
+async function setLocationSettings(
+  latitude,
+  magDeclination,
+  azError,
+  altError,
+  xOffset,
+  yOffset,
+  zOffset
+) {
+  return axios
+    .post(`${config.endpoint}/settings/location`, {
+      latitude,
+      magDeclination,
+      azError,
+      altError,
+      xOffset,
+      yOffset,
+      zOffset
+    })
+    .then(r => r.data);
 }
 
 async function getFlags() {
-  return axios.get(`${config.endpoint}/flags`)
-    .then(r => r.data);
+  return axios.get(`${config.endpoint}/flags`).then(r => r.data);
 }
 
-export { getAllSettings, getAPSettings, getFlags, getLocationSettings, setAPSettings, setLocationSettings };
+export {
+  getAllSettings,
+  getAPSettings,
+  getFlags,
+  getLocationSettings,
+  setAPSettings,
+  setLocationSettings
+};
--- frontend/src/routes/location_settings/index.js
+++ frontend/src/routes/location_settings/index.js
@@ -1,99 +1,138 @@
-import { h, Component } from 'preact';
-import linkState from 'linkstate';
+import { h, Component } from "preact";
+import linkState from "linkstate";
 
-import Button from 'preact-material-components/Button';
-import 'preact-material-components/Button/style.css';
-import TextField from 'preact-material-components/TextField';
-import 'preact-material-components/TextField/style.css';
+import Button from "preact-material-components/Button";
+import "preact-material-components/Button/style.css";
+import TextField from "preact-material-components/TextField";
+import "preact-material-components/TextField/style.css";
 
-import style from './style';
+import style from "./style";
 
-import { getLocationSettings, setLocationSettings } from '../../lib/settings';
+import { getLocationSettings, setLocationSettings } from "../../lib/settings";
 
 export default class LocationSettings extends Component {
-	state = {
-		locationSettings: {
-			latitude: null,
-			magDeclination: null,
-			"x_offset": null,
-      "y_offset": null,
-      "z_offset": null
-		},
-		error: null,
-		info: null,
-	};
+  state = {
+    locationSettings: {
+      latitude: null,
+      magDeclination: null,
+      azError: null,
+      altError: null,
+      xOffset: null,
+      yOffset: null,
+      zOffset: null
+    },
+    error: null,
+    info: null
+  };
 
-	async componentDidMount() {
-		getLocationSettings()
-			.then(r => this.setState({ locationSettings: {...r} }))
-			.catch(e => this.handleError(e));
-	}
-
-	handleError = e => {
-		console.error('problem', e)
-		this.setState({error: e});
-	}
-
-	onSubmit = e => {
-		e.preventDefault();
-		this.setState({error: null, info: null});
-		const { latitude, magDeclination, x_offset, y_offset, z_offset } = this.state.locationSettings;
-		setLocationSettings(latitude, magDeclination, x_offset, y_offset, z_offset)
-			.then(r => this.setState({ info: "Location settings updated." }))
-			.catch(e => this.handleError(e));
+  async componentDidMount() {
+    getLocationSettings()
+      .then(r => this.setState({ locationSettings: { ...r } }))
+      .catch(e => this.handleError(e));
   }
 
-	errorToast() {
-		if (this.state.error != null) {
-			return(
-				<p>
-					{ this.state.error.toString() }
-				</p>
-			)
-		}
-	}
+  handleError = e => {
+    console.error("problem", e);
+    this.setState({ error: e });
+  };
 
-	infoToast() {
-		if (this.state.info != null) {
-			return(
-				<p>
-					{ this.state.info.toString() }
-				</p>
-			)
-		}
-	}
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({ error: null, info: null });
+    const {
+      latitude,
+      magDeclination,
+      xOffset,
+      yOffset,
+      zOffset
+    } = this.state.locationSettings;
+    setLocationSettings(latitude, magDeclination, xOffset, yOffset, zOffset)
+      .then(r => this.setState({ info: "Location settings updated." }))
+      .catch(e => this.handleError(e));
+  };
 
-	render({}, { locationSettings }) {
-		return(
-			<div class={style.ap}>
-				<h1>Location Settings</h1>
-				{this.infoToast()}
-				{this.errorToast()}
-				<form onSubmit={this.onSubmit.bind(this)}>
-					<h2>Basic Settings</h2>
-					<p>
-						<TextField label="Latitude (Decimal degrees)" value={locationSettings.latitude} onInput={linkState(this, 'locationSettings.latitude')}></TextField>
-					</p>
-					<p>
-						<TextField label="Magnetic Declination" value={locationSettings.magDeclination} onInput={linkState(this, 'locationSettings.magDeclination')}></TextField>
-					</p>
+  errorToast() {
+    if (this.state.error != null) {
+      return <p>{this.state.error.toString()}</p>;
+    }
+  }
 
-					<h2>Advanced Settings</h2>
-					<p>
-						Change these settings only if the device does not show zero pitch when laying flat and level and zero degrees pointed in the direction of the magnetic north or south pole.
-					</p>
-					<p>
-						<TextField label="X offset" value={locationSettings.x_offset} onInput={linkState(this, 'locationSettings.x_offset')}></TextField>
-					</p>
-					<p>
-						<TextField label="Y offset" value={locationSettings.y_offset} onInput={linkState(this, 'locationSettings.y_offset')}></TextField>
-					</p>
-					<p>
-						<TextField label="Z offset" value={locationSettings.z_offset} onInput={linkState(this, 'locationSettings.z_offset')}></TextField>
-					</p>
-					<Button raised ripple onClick={this.onSubmit.bind(this)}>Update</Button>
-				</form>
-			</div>
-		)
-	}
-};
+  infoToast() {
+    if (this.state.info != null) {
+      return <p>{this.state.info.toString()}</p>;
+    }
+  }
+
+  render({}, { locationSettings }) {
+    return (
+      <div class={style.ap}>
+        <h1>Location Settings</h1>
+        {this.infoToast()}
+        {this.errorToast()}
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <h2>Basic Settings</h2>
+          <p>
+            <TextField
+              label="Latitude (Decimal degrees)"
+              value={locationSettings.latitude}
+              onInput={linkState(this, "locationSettings.latitude")}
+            ></TextField>
+          </p>
+          <p>
+            <TextField
+              label="Magnetic Declination"
+              value={locationSettings.magDeclination}
+              onInput={linkState(this, "locationSettings.magDeclination")}
+            ></TextField>
+          </p>
+
+          <h2>Advanced Settings</h2>
+          <p>How many degrees over or under is permissable for alignment.</p>
+          <p>
+            <TextField
+              label="Azimuth Error"
+              value={locationSettings.azError}
+              onInput={linkState(this, "locationSettings.azError")}
+            ></TextField>
+          </p>
+          <p>
+            <TextField
+              label="Altitude Error"
+              value={locationSettings.altError}
+              onInput={linkState(this, "locationSettings.altError")}
+            ></TextField>
+          </p>
+          <p>
+            Change these settings only if the device does not show zero pitch
+            when laying flat and level and zero degrees pointed in the direction
+            of the magnetic north or south pole.
+          </p>
+          <p>
+            <TextField
+              label="X offset"
+              value={locationSettings.xOffset}
+              onInput={linkState(this, "locationSettings.xOffset")}
+            ></TextField>
+          </p>
+          <p>
+            <TextField
+              label="Y offset"
+              value={locationSettings.yOffset}
+              onInput={linkState(this, "locationSettings.yOffset")}
+            ></TextField>
+          </p>
+          <p>
+            <TextField
+              label="Z offset"
+              value={locationSettings.zOffset}
+              onInput={linkState(this, "locationSettings.zOffset")}
+            ></TextField>
+          </p>
+          <Button raised ripple onClick={this.onSubmit.bind(this)}>
+            Update
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
